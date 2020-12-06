e8ed91c5a71f4de2e44abd659fada339815bcf2b
Dixit Parmar <Dparmar006@users.noreply.github.com>
2020-12-07 03:02:44 +0530

    About to mess up

--- services/models.py
+++ services/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Service(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.IntegerField()
+    district = models.CharField(max_length=50)
+    address = models.CharField(max_length=150)
+    category = models.CharField(max_length=50)
+    like = models.IntegerField(blank=True, null=True)
+    averageServiceTime = models.IntegerField(blank=True, null=True)
--- users/models.py
+++ users/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 from django.shortcuts import reverse
 from django.contrib.auth.models import AbstractUser
+
+# Other apps 'service'
+from services.models import Service
+
 # Create your models here.
 
 
 class BookonUser(AbstractUser):
-
     class Types(models.TextChoices):
         OWNER = "OWNER", "Owner"
         CUSTOMER = "CUSTOMER", "Customer "
 
-    type = models.CharField(max_length=20,
-                            choices=Types.choices, default=Types.CUSTOMER)
+    type = models.CharField(
+        max_length=20, choices=Types.choices, default=Types.CUSTOMER
+    )
 
     fname = models.CharField(max_length=50)
     lname = models.CharField(max_length=50)
@@ -22,11 +26,50 @@ class BookonUser(AbstractUser):
         return reverse("users:detail", kwargs={"username": self.username})
 
 
+class CustomerManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return (
+            super().get_queryset(*args, **kwargs).filter(type=BookonUser.Types.CUSTOMER)
+        )
+
+
+class OwnerManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(type=BookonUser.Types.OWNER)
+
+
 class Customer(BookonUser):
+    objects = CustomerManager()
+
     class Meta:
         proxy = True
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.type = BookonUser.Types.CUSTOMER
+        return super().save(*args, **kwargs)
+
+
+class CustomerMore(models.Model):
+    district = models.CharField(max_length=50)
+
+
+class OwnerMore(models.Model):
+    service = models.ForeignKey(
+        Service, related_name="Owner of the service", on_delete=models.CASCADE
+    )
 
 
 class Owner(BookonUser):
+    objects = OwnerManager()
+
+    def more(self):
+        return self.ownermore
+
     class Meta:
         proxy = True
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.type = BookonUser.Types.OWNER
+        return super().save(*args, **kwargs)
